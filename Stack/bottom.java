dbca1c3b14093c93d96e0d2ca16f1e0f080593f4
Anuj-Kulkarni <kulkarninanuj10@gmail.com>
2026-03-07 22:20:05 +0530

    Updated

--- Stack/bottom.java
+++ Stack/bottom.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class bottom {
+
+    public static String ReveseStack(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+
+        str = result.toString();
+        return result.toString();
+    }
     public static void main(String[] args) {
-        
+        System.out.println(ReveseStack("abc"));
+        // ReveseStack("b");
+        // ReveseStack("c");
     }
 }
